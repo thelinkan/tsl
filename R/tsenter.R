c9141fbc973288a64ddd43122b0f6aa0b67a8003
Torbjörn Lindquist <thelinkan@gmail.com>
2017-03-05 21:09:04 +0100

    tsenter works, tried to make Rd-file from Roxygen, pdf file still creates badly

--- R/tsenter.R
+++ R/tsenter.R
@@ -1,3 +1,15 @@
+#' Inserts a number to a series
+#'
+#' @param series A ts object
+#' @param year The year for the observation
+#' @param month The month of the observation
+#' @param value The value to insert on year month in the ts object
+#' @return The new ts object.
+#' @examples
+#' temp <- c(1:14)
+#' Series1 <- ts(temp,start=c(2016,1),frequency = 12)
+#' Series1 <- tsenter(series=Series1,year=2017,month=3,value=47)
+
 tsenter <- function(series,year,month,value)
 {
   startper <- start(series)
