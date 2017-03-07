a308e4c057650ffe63da42eb6af517c6f95af289
Torbjörn Lindquist <thelinkan@gmail.com>
2017-03-07 22:56:43 +0100

    Can add two series with the same starting period

--- R/tsadd.R
+++ R/tsadd.R
@@ -14,12 +14,32 @@ tsadd <- function(series1,series2)
 {
   startper1 <- start(series1)
   endper1 <- end(series1)
-
+  length1 <- tslength(series1)
+  
   startper2 <- start(series2)
   endper2 <- end(series2)
+  length2 <- tslength(series2)
+  
+  minlength=min(length1,length2)
+  
 
-  if(startper1[1]==startper2[1] && startper1[1]==startper2[1])
+  if(startper1[1]==startper2[1] && startper1[2]==startper2[2])
   {
-    
+    temp <- c(Series1[1]+Series2[1])
+    year <- startper1[1]
+    month <- startper1[2]
+    SeriesA <- ts(temp,start=c(startper1[1],startper1[2]),frequency=12)
+    for(a in 2:minlength)
+    {
+      month <- month+1
+      if(month>12)
+      {
+        month <- 1
+        year <- year+1
+      }
+      temp <- c(Series1[a]+Series2[a])
+      SeriesA <- tsenter(SeriesA,year,month,temp)
+    }
   }
+  SeriesA
 }
