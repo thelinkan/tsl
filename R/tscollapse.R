92d2f38e9f705f4c8daf96db4ffcda8e08c1df35
Torbjörn Lindquist <thelinkan@gmail.com>
2017-03-07 08:55:01 +0100

    Added tscollapse.R
    Added a short note about what tscollapse.R should do.

---
+++ R/tscollapse.R
@@ -0,0 +1,8 @@
+# Collapses a series from monthly to quarterly or yearly, or from quarterly to yearly.
+# The following methods should be in it:
+#   * Total
+#   * Average
+#   * Minimum
+#   * Maximum
+#   * First
+#   * Last
